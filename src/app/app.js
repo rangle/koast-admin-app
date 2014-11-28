867358be51c9cf3440ab2f1962a6c6230e036496
simon-ramsay <simon@rangle.io>
2014-11-28 11:32:32 -0500

    implemented mock backup functionality
    unit tests are forth coming in the next PR

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,27 @@
 'use strict';
 
-angular.module('koastAdminApp', ['koast'])
-.run(function($log) {
-  $log.debug('Bootstrapped koast admin app.');
-});
+
+angular.module('koastAdminApp', [
+  'koast',
+  'ngRoute',
+  'koastAdminApp.service'
+])
+.config(['$routeProvider',
+    function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'app/main/main.html',
+          controller: 'MainCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }
+  ]);
+
+angular.module('koastAdminApp.service',[])
+.value('ADMIN_DISCOVERY_PATH','TODO')
+.value('KOAST_ROOT','http://TODO/api/')
+.run(['koastAdmin',function(koastAdmin){
+  koastAdmin.load();
+}]);
