92ea30a750cc604cf72df36d8a18a95eb52946e6
Evan <evan@rangle.io>
2015-01-07 15:59:02 -0500

    Update aws-bucket to use koast test

--- src/app/core/constants/constants.js
+++ src/app/core/constants/constants.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('koastAdminApp.core.constants', [])
-  .factory('constants', function() {
+  .factory('constants', function () {
     return {
       adminPath: 'http://localhost:3000/admin',
       // TODO make this a server side configuration option and add endpoints to
       // facilitate selection
-      awsBucket: 'rvaiya-development'
+      awsBucket: 'test-koast'
     };
   });
