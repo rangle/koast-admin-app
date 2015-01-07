4d23a592b323f412ecb87ed891a045f45b7afb73
Evan <evan@rangle.io>
2015-01-07 16:27:08 -0500

    Change status method to pass object instead of nesting it

--- src/app/sections/backup/backup-service.js
+++ src/app/sections/backup/backup-service.js
@@ -8,34 +8,39 @@
 angular.module('koastAdminApp.sections.backup.backup-service', [])
   .service('backup', function (api, constants) {
     var service = {
-        //make create backup request
-        //where name is the name of the backup and type is what kind of backup it will be
-        createBackup: function (name, collections, type) {
-            return api.callMethod('backup', 'create', {
-              collections: collections,
-              type: type,
-              name: name,
-              opts: { bucket: constants.awsBucket }
-            });
-        },
+      //make create backup request
+      //where name is the name of the backup and type is what kind of backup it will be
+      createBackup: function (name, collections, type) {
+        return api.callMethod('backup', 'start', {
+          collections: collections,
+          type: type,
+          name: name,
+          opts: {
+            name: name,
+            bucket: constants.awsBucket
+          }
+        });
+      },
 
-        //ask for the status of a particular inprogress backup
-        //where id is the id from the backup receipt
-        status: function (id) {
-            return api.callMethod('backup', 'stat', { id: id });
-        },
+      //ask for the status of a particular inprogress backup
+      //where id is the id from the backup receipt
+      status: function (id) {
+        return api.callMethod('backup', 'stat', id);
+      },
 
-        //get a list of backups
-        list: function () {
-            return api.callMethod('backup', 'list');
-        },
+      //get a list of backups
+      list: function () {
+        return api.callMethod('backup', 'list');
+      },
 
-        //restore the db using the selected backup
-        //where id is the id of the backup
-        restoreBackup: function (id) {
-            return api.callMethod('backup', 'restore', { id: id });
-        }
+      //restore the db using the selected backup
+      //where id is the id of the backup
+      restoreBackup: function (id) {
+        return api.callMethod('backup', 'restore', {
+          id: id
+        });
+      }
     };
 
     return service;
-});
+  });
