dc4f8add4cda2c26eca5b8be4420d1a59e24f163
Christine Davis <christine@rangle.io>
2015-01-14 12:12:01 -0500

    Convert tabs to spaces.
    [#85706310]

--- src/app/sections/backup/backup-controller.test.js
+++ src/app/sections/backup/backup-controller.test.js
@@ -1,41 +1,41 @@
 'use strict';
 
 describe('backupCtrl:', function () {
-	var backupCtrl,
-		tagList = {},
-		backupRecord = {
-			backupId: '34072750-9b3a-11e4-adf0-8b69b77a50cb',
-			receipts: [
-				{
-					collection: 'tasks',
-					data: {
-						bucket: 'mock-bucket',
-						key: 'mock'
-					}
-				},
-				{
-					collection: 'users',
-					data: {
-						bucket: 'mock-bucket',
-						key: 'mock'
-					}
-				}
-			]
-		};
+  var backupCtrl,
+    tagList = {},
+    backupRecord = {
+      backupId: '34072750-9b3a-11e4-adf0-8b69b77a50cb',
+      receipts: [
+        {
+          collection: 'tasks',
+          data: {
+            bucket: 'mock-bucket',
+            key: 'mock'
+          }
+        },
+        {
+          collection: 'users',
+          data: {
+            bucket: 'mock-bucket',
+            key: 'mock'
+          }
+        }
+      ]
+    };
 
-	beforeEach(module('koastAdminApp.sections.backup.backup-controller'));
+  beforeEach(module('koastAdminApp.sections.backup.backup-controller'));
 
   beforeEach(inject(function ($controller, $interval, $q) {
-		var mockBackup = {
-			list: function () {
-				return $q.when([backupRecord]);
-			},
-			restoreBackup: function (id) {
-				var deferred = $q.defer();
-				deferred.resolve('done');  // this is what seems to happen currently.
-				return deferred.promise;
-			}
-		};
+    var mockBackup = {
+      list: function () {
+        return $q.when([backupRecord]);
+      },
+      restoreBackup: function (id) {
+        var deferred = $q.defer();
+        deferred.resolve('done');  // this is what seems to happen currently.
+        return deferred.promise;
+      }
+    };
 
     backupCtrl = $controller('backupCtrl', {
       $interal: $interval,
@@ -44,41 +44,41 @@ describe('backupCtrl:', function () {
     });
   }));
 
-	it('should show a dialog when restore is clicked', function () {
-		expect(backupCtrl.confirmingRestore).to.be.false();
-		expect(backupCtrl.restoringBackup).to.be.not.ok();
+  it('should show a dialog when restore is clicked', function () {
+    expect(backupCtrl.confirmingRestore).to.be.false();
+    expect(backupCtrl.restoringBackup).to.be.not.ok();
 
-		backupCtrl.confirmRestore(backupRecord);
+    backupCtrl.confirmRestore(backupRecord);
 
-		expect(backupCtrl.confirmingRestore).to.be.true();
-		expect(backupCtrl.restoringBackup).to.be.not.ok();
-		expect(backupCtrl.toRestore).to.be.equal(backupRecord);
-	});
+    expect(backupCtrl.confirmingRestore).to.be.true();
+    expect(backupCtrl.restoringBackup).to.be.not.ok();
+    expect(backupCtrl.toRestore).to.be.equal(backupRecord);
+  });
 
-	describe('verify correct effects happen from backup restore confirmation dialog', function () {
-		beforeEach(function () {
-			backupCtrl.toRestore = backupRecord;
-			backupCtrl.confirmingRestore = true;			
-		});
+  describe('verify correct effects happen from backup restore confirmation dialog', function () {
+    beforeEach(function () {
+      backupCtrl.toRestore = backupRecord;
+      backupCtrl.confirmingRestore = true;      
+    });
 
-		it('should start restoring when OK is clicked in the confirmation dialog', function () {
-			expect(backupCtrl.restoringBackup).to.be.not.ok();
+    it('should start restoring when OK is clicked in the confirmation dialog', function () {
+      expect(backupCtrl.restoringBackup).to.be.not.ok();
 
-			backupCtrl.restoreBackup(backupRecord.backupId);
+      backupCtrl.restoreBackup(backupRecord.backupId);
 
-			expect(backupCtrl.confirmingRestore).to.be.false();
-			expect(backupCtrl.restoringBackup).to.be.true();
-			expect(backupCtrl.toRestore).to.be.equal(backupRecord);
-		});
+      expect(backupCtrl.confirmingRestore).to.be.false();
+      expect(backupCtrl.restoringBackup).to.be.true();
+      expect(backupCtrl.toRestore).to.be.equal(backupRecord);
+    });
 
-		it('should not start restoring when Cancel is clicked in the confirmation dialog', function () {
-			expect(backupCtrl.restoringBackup).to.be.not.ok();
+    it('should not start restoring when Cancel is clicked in the confirmation dialog', function () {
+      expect(backupCtrl.restoringBackup).to.be.not.ok();
 
-			backupCtrl.cancelRestore();
+      backupCtrl.cancelRestore();
 
-			expect(backupCtrl.confirmingRestore).to.be.false();
-			expect(backupCtrl.restoringBackup).to.be.not.ok();
-			expect(backupCtrl.toRestore).to.be.empty();
-		});
-	});
+      expect(backupCtrl.confirmingRestore).to.be.false();
+      expect(backupCtrl.restoringBackup).to.be.not.ok();
+      expect(backupCtrl.toRestore).to.be.empty();
+    });
+  });
 });
